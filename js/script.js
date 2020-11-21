14ef46132fefe9bad53755ad4b6eedb550990ef8
Daniel Murphy <murphyda@protonmail.com>
2020-11-21 16:46:03 +0100

    Add opening and closing of files

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+/*
+
 ! function ($) {
     var OS = {
         init: function() {
@@ -9,5 +11,37 @@
             setTimeout(this.setBodyHeight, 100)
         }
     };
-    OS.init();
+    OS.init(),
+
+    $("#readmebut").on("click", (function(e) {
+        alert(1);
+        e.preventDefault(), e.stopPropagation(), $(".window.readmetxt").addClass("focus").show("slow");
+    }));
+
+
 } (jQuery);
+*/
+
+
+$(document).ready(function(){
+    var OS = {
+        init: function() {
+            var self = this;
+            self.setBodyHeight();
+        },
+        setBodyHeight: function() {
+            $("body").css("height", window.innerHeight), 
+            setTimeout(this.setBodyHeight, 100)
+        }
+    };
+    OS.init(),
+
+    $("#readmebut").on("click", (function() {
+        $(".window.readmetxt").addClass("focus").show("slow");
+    })),
+    $(".window .close").on("click", (function() {
+        $(this).parent().parent().hide();
+    }))
+    
+    ;
+  });
